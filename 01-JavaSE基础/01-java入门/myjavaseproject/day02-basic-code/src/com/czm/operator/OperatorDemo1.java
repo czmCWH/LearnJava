ee32efb2e34603e5f3b246b62a04f902ec13e7f2
czm <zhengmao.chen@upai.com>
2025-03-09 16:09:06 +0800

    add

--- 01-JavaSE基础/01-java入门/myjavaseproject/day02-basic-code/src/com/czm/operator/OperatorDemo1.java
+++ 01-JavaSE基础/01-java入门/myjavaseproject/day02-basic-code/src/com/czm/operator/OperatorDemo1.java
@@ -5,8 +5,12 @@ public class OperatorDemo1 {
         /*
         1、基本运算符
         注意：
-            两个整数做除法，其结果一定是整数，因为最高类型是整数。
+            ⚠️：两个整数做除法，其结果一定是整数，因为最高类型是整数。
             与字符串做+运算时会被当成连接符，其结果还是字符串。
+
+        'a' 的 Unicode 值是 97（ASCII 码兼容）。
+        'A' 对应 65，'0' 对应 48
+
         */
         int i = 5;
         int j = 2;
@@ -20,14 +24,21 @@ public class OperatorDemo1 {
 
         /*
          2、+ 运算符可以做连接符
-         + 符号与 字符串 运算的时候是用作连接符的，其结果依然是个 字符串。
+         + 符号与【字符串】运算的时候是用作连接符的，其结果依然是个 字符串。
          */
         int a = 20;
         System.out.println("你的年纪是：" + a);
         System.out.println("你的年纪是：" + a + '岁'); // 打印：你的年纪是：20岁
         System.out.println(a + 'a' + "大小");     // 打印：117大小，因为字符a的编号是97，
-        System.out.println('a' + 1);    // 打印：98
+        /*
+           当 char 参与算术运算时，如下会触发 自动类型提升：
+            char ➔ int（因为 char 是 16 位，int 是 32 位）。
+            byte/short 也会提升为 int。
+         */
+        System.out.println('a' + 1);    // 打印：98，因为 char与int进行运算时，会触发自动类型提升，
         System.out.println("a" + 1);    // 打印：a1
+        // 若希望输出字符 'b'，需显式将结果强制转换回 char：
+        System.out.println((char)('a' + 1));  // 输出 b
 
         /*
          3、自增、自减运算符
